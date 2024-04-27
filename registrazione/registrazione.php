c6b85ba4e1258056eff5e20b6e8ee0436aadf2df
enricobosco <bosco.1956727@studenti.uniroma1.it>
2024-04-27 18:33:03 +0200

    aggiornamento form di ricerca auto

--- registrazione/registrazione.php
+++ registrazione/registrazione.php
@@ -2,7 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] != "POST") {
     header("Location: /");
 } else {
-    $dbconn = pg_connect("host=localhost port=5433 dbname=EsempioLogin user=postgres password=password")
+    $dbconn = pg_connect("host=localhost port=5432 dbname=utenti user=postgres password=Lukakuinter9")
         or die('Could not connect: ' . pg_last_error());
 }
 ?>
@@ -13,7 +13,7 @@ if ($_SERVER["REQUEST_METHOD"] != "POST") {
     <?php
     if ($dbconn) {
         $email = $_POST['inputEmail'];
-        $q1 = "SELECT * FROM organizzazione WHERE email=$1";
+        $q1 = "SELECT * FROM utente WHERE email=$1";
         $result = pg_query_params($dbconn, $q1, array($email));
         if ($tuple = pg_fetch_array($result, null, PGSQL_ASSOC)) {
             echo "<h1> Spiacente, l'indirizzo email non è disponibile </h1>
@@ -21,10 +21,9 @@ if ($_SERVER["REQUEST_METHOD"] != "POST") {
         } else {
             $nome = $_POST['inputName'];
             $cognome = $_POST['inputSurname'];
-            $cap = $_POST['inputCap'];
             $password = password_hash($_POST['inputPassword'], PASSWORD_DEFAULT);
-            $q2 = "INSERT INTO utente VALUES ($1, $2, $3, $4, $5)";
-            $data = pg_query_params($dbconn, $q2, array($email, $nome, $cognome, $password, $cap));
+            $q2 = "INSERT INTO utente VALUES ($1, $2, $3, $4)";
+            $data = pg_query_params($dbconn, $q2, array($email, $password, $nome, $cognome));
             if ($data) {
                 echo "<h1> Registrazione completata. Puoi iniziare ad usare il sito <br/></h1>";
                 echo "<a href='../login'> Clicca qui </a> per loggarti!";

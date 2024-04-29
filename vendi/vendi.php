9d3dce42c6d3c70fa9a4c388f03fd01417e6790e
enricobosco <bosco.1956727@studenti.uniroma1.it>
2024-04-29 20:36:52 +0200

    aggiornamento vendi.php

--- vendi/vendi.php
+++ vendi/vendi.php
@@ -10,26 +10,22 @@ if ($_SERVER["REQUEST_METHOD"] != "POST") {
 if ($dbconn) {
     // Gestione dell'upload delle immagini
     $targetDirectory = "uploads/"; // Directory di destinazione per le immagini
-    $uploadedFiles = array(); // Array per memorizzare i percorsi dei file caricati
+    $uploadedFile = ""; // Percorso del file caricato
 
-    // Loop attraverso ogni file caricato
-    foreach ($_FILES["foto"]["tmp_name"] as $key => $tmp_name) {
-        // Ottieni il nome del file e il percorso temporaneo
-        $fileName = $_FILES["foto"]["name"][$key];
-        $fileTmpName = $_FILES["foto"]["tmp_name"][$key];
-        
-        // Crea il percorso di destinazione per il file caricato
-        $targetPath = $targetDirectory . basename($fileName);
-        
-        // Sposta il file dalla directory temporanea alla directory di destinazione
-        if (move_uploaded_file($fileTmpName, $targetPath)) {
-            // Aggiungi il percorso del file all'array degli upload
-            $uploadedFiles[] = $targetPath;
-        } else {
-            echo "Si è verificato un errore durante il caricamento del file $fileName.<br>";
-        }
+    // Ottieni il nome del file e il percorso temporaneo del primo file caricato
+    $fileName = $_FILES["foto"]["name"];
+    $fileTmpName = $_FILES["foto"]["tmp_name"];
+
+    // Crea il percorso di destinazione per il file caricato
+    $targetPath = $targetDirectory . basename($fileName);
+
+    // Sposta il file dalla directory temporanea alla directory di destinazione
+    if (move_uploaded_file($fileTmpName, $targetPath)) {
+        // Aggiungi il percorso del file agli upload
+        $uploadedFile = $targetPath;
+    } else {
+        echo "Si è verificato un errore durante il caricamento del file $fileName.<br>";
     }
-
     // Altri dati dal modulo
     $marca = $_POST['marca'];
     $modello = $_POST['modello'];
@@ -41,14 +37,17 @@ if ($dbconn) {
     $carburante = $_POST['carburante'];
     $cambio = $_POST['cambio'];
     $potenza = $_POST['potenza'];
+    $foto = $uploadedFile;
     $descrizione = $_POST['descrizione'];
+    echo "aaaa$foto";
 
     // Query SQL per l'inserimento dei dati nella tabella Auto
     $query = "INSERT INTO annuncio (marca, modello, prezzo, trattabile, carrozzeria, anno, chilometraggio, carburante, cambio, potenza, foto, descrizione) 
-              VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)";
-    
+          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)";
+
     // Esecuzione della query con i parametri
-    $result = pg_query_params($dbconn, $query, array($marca, $modello, $prezzo, $trattabile, $carrozzeria, $anno, $chilometraggio, $carburante, $cambio, $potenza, $uploadedFiles, $descrizione));
+    $result = pg_query_params($dbconn, $query, array($marca, $modello, $prezzo, $trattabile, $carrozzeria, $anno, $chilometraggio, $carburante, $cambio, $potenza, $foto, $descrizione));
+
     
     if ($result) {
         echo "<h1>Dati inseriti correttamente nella tabella Auto</h1>";
